2cf80739b37e146f3804c791bb41ff250e2f6413
Julio Collado <jcollado@altimetrik.com>
2019-12-12 10:14:13 -0300

    Initial Commit

---
+++ SideMenu Demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu Demo2
+//
+//  Created by Julio Collado on 12/12/19.
+//  Copyright © 2019 Julio Collado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
